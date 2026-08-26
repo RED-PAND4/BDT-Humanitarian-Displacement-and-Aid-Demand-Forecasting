ed535cf5c5870c81bb8c8758eb86e470777c29f6
francescabelli-netizen <francesca.belli@studenti.unitn.it>
2026-08-26 17:49:42 +0200

    cleaning
    Removed files for: currency, food prices market monitor, wfp commodity, wfp market, org, org type
    Removed old silver to gold

--- ingestion/bronze/kafka-to-bronze_hdx_idps.py
+++ ingestion/bronze/kafka-to-bronze_hdx_idps.py
@@ -1,12 +1,10 @@
 import sys
+import os
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.functions import col, from_json, regexp_replace, trim, current_timestamp
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType, TimestampType, BooleanType
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 from typing import Dict, List, Optional
 
-import time
-import sys
-import os
 
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(parent_dir)
@@ -15,15 +13,15 @@ from utilities import get_spark_session, parse_kafka_message, initialize_delta_t
 
 
 KAFKA_BROKER = "kafka:9092"
-KAFKA_TOPIC = "food_prices_market_monitor"
-
+# Questo deve corrispondere al topic usato nel file main_hdx_idps.py
+KAFKA_TOPIC = "idps"
 
 if __name__ == "__main__":
-    spark = get_spark_session("KafkaToBronze-FPMM")
+    spark = get_spark_session("KafkaToBronze_HDX_IDPs")
     #print("Clearing stale catalog metadata...")
     #spark.sql("DROP TABLE IF EXISTS default.test1")
     
-    # Define schema of expected JSON message matching the complete API response
+# Define schema of expected JSON message per HDX IDPs
     schema = StructType([
         StructField("location_code", StringType(), True),
         StructField("location_name", StringType(), True),
@@ -33,23 +31,14 @@ if __name__ == "__main__":
         StructField("admin2_name", StringType(), True),
         StructField("admin_level", IntegerType(), True),
         StructField("resource_hdx_id", StringType(), True),
-        StructField("market_code", StringType(), True),
-        StructField("market_name", StringType(), True),
-        StructField("commodity_code", StringType(), True),
-        StructField("commodity_name", StringType(), True),
-        StructField("commodity_category", StringType(), True),
-        StructField("currency_code", StringType(), True),
-        StructField("unit", StringType(), True),
-        StructField("price_flag", StringType(), True),
-        StructField("price_type", StringType(), True),
-        StructField("price", DoubleType(), True),
-        StructField("lat", DoubleType(), True),
-        StructField("lon", DoubleType(), True),
-        StructField("reference_period_start", TimestampType(), True),
-        StructField("reference_period_end", TimestampType(), True)
+        StructField("reporting_round", IntegerType(), True),
+        StructField("assessment_type", StringType(), True),
+        StructField("operation", StringType(), True),
+        StructField("population", IntegerType(), True),
+        StructField("reference_period_start", StringType(), True),
+        StructField("reference_period_end", StringType(), True)
     ])
 
-    # Maintaining exact variable names as keys and values from the API
     my_fields_to_keep = {
         "location_code": "location_code",
         "location_name": "location_name",
@@ -59,27 +48,18 @@ if __name__ == "__main__":
         "admin2_name": "admin2_name",
         "admin_level": "admin_level",
         "resource_hdx_id": "resource_hdx_id",
-        "market_code": "market_code",
-        "market_name": "market_name",
-        "commodity_code": "commodity_code",
-        "commodity_name": "commodity_name",
-        "commodity_category": "commodity_category",
-        "currency_code": "currency_code",
-        "unit": "unit",
-        "price_flag": "price_flag",
-        "price_type": "price_type",
-        "price": "price",
-        "lat": "lat",
-        "lon": "lon",
+        "reporting_round": "reporting_round",
+        "assessment_type": "assessment_type",
+        "operation": "operation",
+        "population": "population",
         "reference_period_start": "reference_period_start",
         "reference_period_end": "reference_period_end"
-        # Add or remove other fields from the API as needed
     }
 
     initialize_delta_table(
         spark=spark,
         db_name="bronze",
-        table_name="foodpricesmarketmonitor"
+        table_name="idps"
     )
     print("Starting Kafka Read Stream...")
 
@@ -89,40 +69,41 @@ if __name__ == "__main__":
         .format("kafka")
         .option("kafka.bootstrap.servers", KAFKA_BROKER)
         .option("subscribe", KAFKA_TOPIC)
-        .option("startingOffsets", "earliest")
+        .option("startingOffsets", "earliest") # earliest per avviare da zero, latest per processare solo nuovi dati
+        #.option("startingOffsets", "latest")
         .load()
     )
     print("Parse Kafka Read Stream...")
+    
     # Transform: Parse and Clean
     parsed_df = parse_kafka_message(
         df=raw_df,
         schema=schema,
         fields_mapping=my_fields_to_keep
     )
-    # target_columns = list(my_fields_to_keep.values())
+    ## target_columns = list(my_fields_to_keep.values())
     parsed_df = parsed_df.withColumn("ingested_at", current_timestamp())
     
     
     # 3. Updated target columns list to include the new column
     target_columns = list(my_fields_to_keep.values()) + ["ingested_at"]
+    
 
     print("Starting Write Streams...")
 
-    # Sink 1: Write to Console (for debugging/testing)
-    # console_query = (
-    #     parsed_df.writeStream
-    #     .format("console")
-    #     .outputMode("append")
-    #     .option("truncate", "false")
-    #     .trigger(availableNow=True)
-    #     .start()
-    # )
-    # console_query.stop()
+    #Sink 1: Write to Console (for debugging/testing)
+    console_query = (
+        parsed_df.writeStream
+        .format("console")
+        .outputMode("append")
+        .option("truncate", "false")
+        .start()
+    )
 
 
 
     # Define a path for Spark to track streaming progress
-    CHECKPOINT_PATH = "s3a://lakehouse/checkpoints/bronze/foodpricesmarketmonitor"
+    CHECKPOINT_PATH = "s3a://lakehouse/checkpoints/bronze_idps"
 
     print("Writing stream to Delta Lake...")
     delta_query = (
@@ -131,10 +112,11 @@ if __name__ == "__main__":
         .option("mergeSchema", "true")
         .outputMode("append")
         .option("checkpointLocation", CHECKPOINT_PATH)
-        #.trigger(processingTime="10 seconds")  # Adjust trigger interval as needed
         .trigger(availableNow=True)
+        #.trigger(processingTime="10 seconds")  # Adjust trigger interval as needed
         #.option("maxOffsetsPerTrigger", "50")
-        .start("s3a://lakehouse/bronze/foodpricesmarketmonitor")
+        .start("s3a://lakehouse/bronze/idps")
+        #.start()
     )
 
 
@@ -143,7 +125,3 @@ if __name__ == "__main__":
     #spark.stop()
     # Wait for the streams to process data indefinitely
     delta_query.awaitTermination()
-    
-    print("Execution complete. Explicitly shutting down Spark to release locks...")
-    spark.stop()
-    sys.exit(0)
